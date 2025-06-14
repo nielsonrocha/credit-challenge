91f79a3abeaa0576bf85b0d2709884e8be30ce0e
nielsonrocha <nielsonrocha@gmail.com>
2025-06-13 21:53:32 -0300

    feat: Commit inicial do backend

---
+++ backend/src/test/java/com/github/nielsonrocha/credit/CreditChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nielsonrocha.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
