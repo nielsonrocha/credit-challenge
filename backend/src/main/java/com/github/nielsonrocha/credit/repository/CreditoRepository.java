8d425e39a5ef75dae29873a37c00dfc6d56b46e7
nielsonrocha <nielsonrocha@gmail.com>
2025-06-13 23:38:17 -0300

    feat: criacao de consultas usando numero da nfse e numero do credito

--- backend/src/main/java/com/github/nielsonrocha/credit/repository/CreditoRepository.java
+++ backend/src/main/java/com/github/nielsonrocha/credit/repository/CreditoRepository.java
@@ -1,10 +1,17 @@
 package com.github.nielsonrocha.credit.repository;
 
 import com.github.nielsonrocha.credit.entity.Credito;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CreditoRepository extends JpaRepository<Credito, Long> {
-    
+
+    @Query("select c from Credito c where c.numeroNfse = ?1 order by c.dataConstituicao desc")
+    List<Credito> findByNumeroNfse(String numeroNfse);
+
+    Optional<Credito> findByNumeroCredito(String numeroCredito);
 }
