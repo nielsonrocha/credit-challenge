db201847056a2381ac96f5d5a659e8d8ebcdb8c4
nielsonrocha <nielsonrocha@gmail.com>
2025-06-17 10:17:23 -0300

    feat: adicionar suporte a múltiplas origens CORS na configuração da API

--- backend/src/main/java/com/github/nielsonrocha/credit/config/WebConfig.java
+++ backend/src/main/java/com/github/nielsonrocha/credit/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/api/**")
-        .allowedOrigins("http://localhost:4200")
+        .allowedOrigins("http://localhost:4200", "http://localhost")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("Authorization", "Content-Type", "Accept")
         .maxAge(3600);
